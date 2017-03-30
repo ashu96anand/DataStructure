44fb9da54955001051a5ff1261185a5fac1dfbd8
Ashutosh Anand <ashu@Ashutoshs-MacBook-Pro.local>
2017-03-31 01:22:24 +0530

    Added BubbleSort

---
+++ src/arrays/BubbleSort.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] inp = {5,4,3,2,1};
+		bubbleSort(inp);
+		System.out.println(Arrays.toString(inp));
+
+
+	}
+	// 5 4 3 2 1
+	public static void bubbleSort(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            for (int j = 1; j < arr.length-i; j++){
+                if (arr[j-1] > arr[j])
+                {
+                    
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            
+        }
+        
+    }
+}
